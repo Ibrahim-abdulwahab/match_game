364279d221c117189230e9c6ab2dac9d8e669258
[Ibrahim Abdulwahab] <[ibrahim.abdulwahab1997@gmail.com]>
2022-09-16 15:30:00 +0300

    build the win condition

--- script.js
+++ script.js
@@ -7,17 +7,29 @@ window.addEventListener('load', function(){
    const card6=document.getElementById("card-six");
    const scoreboard=document.getElementById("score");
    let htmlmatch = 0;
+   let cssmatch=0;
+   let jsmatch=0;
+   let wrongmatch=0;
    let score=0;
     
-
-
+   const correct = (x,y) =>{
+    score=score+5;
+    scoreboard.innerHTML=`Score: ${score}`;
+    setTimeout(() => { x.className="card-gone"; y.className="card-gone"; }, 1000);
+    wrongmatch=0;
+   }
+   
    const card1click = () =>{
     card1.className="card-html";
     htmlmatch=htmlmatch+1;
+    wrongmatch=wrongmatch+1
     if (htmlmatch==2){
-        score=score+5;
-        scoreboard.innerHTML=`Score: ${score}`;
-        setTimeout(() => { card1.className="card-gone"; card6.className="card-gone"; }, 1000);
+        correct(card1,card6)
+    }
+    if (wrongmatch=2){
+        card1.className="card"
+        wrongmatch=1;
+        htmlmatch=0;
     }
 }
    card1.addEventListener("click",card1click)
@@ -26,12 +38,49 @@ window.addEventListener('load', function(){
     card6.className="card-html";
     htmlmatch=htmlmatch+1;
     if (htmlmatch==2){
-        score=score+5;
-        scoreboard.innerHTML=`Score: ${score}`;
-        setTimeout(() => { card1.className="card-gone"; card6.className="card-gone"; }, 1000);
+        correct(card1,card6)
     }
 }
    card6.addEventListener("click",card6click)
+
+   const card2click = () =>{
+    card2.className="card-css";
+    cssmatch=cssmatch+1;
+    if (cssmatch==2){
+        correct(card2,card4)
+    }
+}
+   card2.addEventListener("click",card2click)
+
+   const card4click = () =>{
+    card4.className="card-css";
+    cssmatch=cssmatch+1;
+    if (cssmatch==2){
+        correct(card2,card4)
+    }
+}
+   card4.addEventListener("click",card4click)
+
+   const card3click = () =>{
+    card3.className="card-js";
+    jsmatch=jsmatch+1;
+    if (jsmatch==2){
+        correct(card3,card5)
+    }
+}
+   card3.addEventListener("click",card3click)
+
+   const card5click = () =>{
+    card5.className="card-js";
+    jsmatch=jsmatch+1;
+    if (jsmatch==2){
+        correct(card3,card5)
+    }
+}
+   card5.addEventListener("click",card5click)
+
+
+
    
 
    
