c9ea78d0e5ec022768939fa8f3ee52eb26070154
[Ibrahim Abdulwahab] <[ibrahim.abdulwahab1997@gmail.com]>
2022-09-16 14:35:31 +0300

    matching card1 and card6 works

--- script.js
+++ script.js
@@ -5,9 +5,30 @@ window.addEventListener('load', function(){
    const card4=document.getElementById("card-four");
    const card5=document.getElementById("card-five");
    const card6=document.getElementById("card-six");
-
-   const card1click = () =>{card1.className="card-html"}
+   let htmlmatch = 0;
+   let score=0;
+   const card1click = () =>{
+    card1.className="card-html";
+    htmlmatch=htmlmatch+1;
+    if (htmlmatch==2){
+        score=score+5;
+        card1.className="card-gone";
+        card6.className="card-gone"
+    }
+}
    card1.addEventListener("click",card1click)
+
+   const card6click = () =>{
+    card6.className="card-html";
+    htmlmatch=htmlmatch+1;
+    if (htmlmatch==2){
+        score=score+5;
+        card1.className="card-gone";
+        card6.className="card-gone"
+    }
+}
+   card6.addEventListener("click",card6click)
+
 
    
 
